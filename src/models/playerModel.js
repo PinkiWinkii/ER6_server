fb09d5bc643f82ebb9e162f7cc6844b6aa0b1843
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-09-25 09:46:09 +0200

    [ER6-36][NEW]CREATE DATA SCHEME FOR THE PLAYERS COLLECTION FOR MONGODB

---
+++ src/models/playerModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const playerSchema = new Schema({
+    attributes:     {},
+    equipment:      {},
+    inventory:      {},
+    name:           String,
+    nickname:       String,
+    email:          String,
+    level:          Number,
+    experience:     Number,
+    is_active:      Boolean,
+    profile:        {},
+    tasks:          [],
+    gold:           Number,
+    created_date:   String
+
+})
+
+// Export model
+module.exports = mongoose.model('Player', playerSchema);
