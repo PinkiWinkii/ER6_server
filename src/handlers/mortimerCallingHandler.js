5f92bbddeefde635aaa83773d9a8e8ff050bb85d
llabaka <lander.labaka@ikasle.aeg.eus>
2024-11-20 10:07:28 +0100

    [ER6-196][DELETE] Unnecesary code in mortimerCallinghandler file

--- src/handlers/mortimerCallingHandler.js
+++ src/handlers/mortimerCallingHandler.js
@@ -1,25 +1,5 @@
+const { sendPushNotification } = require('../notifications/notificationSender');
 const playerService = require('../services/playerService');
-const { getMessaging } = require('firebase-admin/messaging');
-
-const sendPushNotification = async (fcmToken, title, body) => {
-    const message = {
-    token: fcmToken,
-    notification: {
-        title: title,
-        body: body,
-    },
-    android: {
-        "direct_boot_ok": true,
-    },
-    };
-
-    try {
-    const response = await getMessaging().send(message);
-    console.log("Notificación enviada:", response);
-    } catch (error) {
-    console.error("Error enviando notificación:", error);
-    }
-    };
 
 const mortimerCallingHandler = (socket) => {
 
