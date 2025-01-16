af0349f204cca0bd7c34b45911445cc90f3e5481
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2025-01-16 11:51:29 +0100

    [UPDATE] PlayerBetrayerHandler

--- src/handlers/playerBetrayHandler.js
+++ src/handlers/playerBetrayHandler.js
@@ -4,20 +4,14 @@ const playerBetrayerHandler = (socket, io) => {
 
     socket.on('UpdateBetrayer', async (value) => {
 
-        //Hacer fetch a kaotika
-        const updateResponse = await fetch(`https://kaotika-server.fly.dev/players/loyalty/email/${value.email}`, {
-            method: 'PATCH'
-        });
+        const data = {
+            isBetrayer: true
+        }
 
-        console.log('RESPONSE IN PATCH PLAYER IN KAOTIKA:');
-        console.log(updateResponse);
-    
-        const data = await updateResponse.data.json();
-
-        console.log('RESPONSE IN PATCH PLAYER IN KAOTIKA:');
+        console.log('RESPONSE JSON DATA IN PATCH PLAYER IN KAOTIKA:');
         //Añades al json del patch tanto el oro como el inventario
         console.log(data);
-        
+
 
         const updatePlayer = await playerService.updateOnePlayerIsBetrayer(value._id, data);
         io.emit('IsBetrayer', updatePlayer);
